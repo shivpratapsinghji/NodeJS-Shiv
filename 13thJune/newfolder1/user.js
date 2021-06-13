3f97d3be496670cb3b890abaf44f24ff3f358aa9
Shiv Pratap Singh <73564579+shivpratapsinghji@users.noreply.github.com>
2021-06-13 18:12:50 +0530

    Delete user.js

--- 13thJune/newfolder1/user.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require('mongoose');
-let userSchema=new mongoose.Schema({
-      _id:mongoose.Schema.Types.ObjectId,
-      email:String,
-      name:String
-});
-module.exports=mongoose.model('user',userSchema);
